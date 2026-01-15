5b9f1572c277fe752089e7949f76f67efb84f20e
James Reilly <jreilly1821@gmail.com>
2026-01-15 16:35:20 -0600

    fix: create dummy repo files for rpmbuild on almalinux

--- build_files/common/build-ublue-os-akmods-addons.sh
+++ build_files/common/build-ublue-os-akmods-addons.sh
@@ -5,7 +5,19 @@ set "${CI:+-x}" -euo pipefail
 
 ### BUILD UBLUE AKMODS-ADDONS RPM
 # ensure a higher priority is set for our ublue akmods COPR to pull deps from it over other sources (99 is default)
-echo "priority=85" >> /tmp/ublue-os-akmods-addons/rpmbuild/SOURCES/_copr_ublue-os-akmods.repo
+REPO_FILE="/tmp/ublue-os-akmods-addons/rpmbuild/SOURCES/_copr_ublue-os-akmods.repo"
+NEG_REPO_FILE="/tmp/ublue-os-akmods-addons/rpmbuild/SOURCES/negativo17-fedora-multimedia.repo"
+
+if [ -f "$REPO_FILE" ]; then
+    echo "priority=85" >> "$REPO_FILE"
+else
+    # Create dummy files for EL (CentOS/AlmaLinux) where these repos are skipped
+    touch "$REPO_FILE"
+fi
+
+if [ ! -f "$NEG_REPO_FILE" ]; then
+     touch "$NEG_REPO_FILE"
+fi
 
 install -D /etc/pki/akmods/certs/public_key.der /tmp/ublue-os-akmods-addons/rpmbuild/SOURCES/public_key.der
 rpmbuild -ba \
